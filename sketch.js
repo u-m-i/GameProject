a23bc060256b8d2faa3f522ddd074a061b63b7b1
Miguel Reyes <asiessoyeltier1@gmail.com>
2023-12-17 23:44:52 -0500

    [feature] Canyon falling

--- sketch.js
+++ sketch.js
@@ -6,14 +6,18 @@ const CHEST_LENGTH = (SPINE_LENGTH / 2) + 2;
 const HAND_LENGTH = 12
 const LEG_LENGTH = 14;
 
+let delta = 10
 let offset = 18;
+let maximumHeight;
 
 let gameChar_x;
 let gameChar_y;
 let floorPos_y;
+let coin;
+let canyon;
 
 // Booleans
-let isFalling = false, isPlummeting = false;
+let isFalling = false, isJumping = false, isPlummeting = false;
 let isLeft = false, isRight = false;
 
 function setup()
@@ -22,6 +26,24 @@ function setup()
 	floorPos_y = height * 3/4;
 	gameChar_x = width/2;
 	gameChar_y = floorPos_y;
+    maximumHeight = floorPos_y - 60;
+
+    coin = 
+        {
+            x : 450, 
+            y : floorPos_y - 10, 
+            isPicked : false,
+        };
+
+    canyon = 
+        {
+            x : 120,
+            y : floorPos_y,
+            limit : 80,
+            size : 72,
+            hasEntered : false
+        };
+
 }
 
 function draw()
@@ -30,29 +52,104 @@ function draw()
 	///////////DRAWING CODE//////////
 
 	background(100,155,255); //fill the sky blue
+    //console.log(gameChar_x);
 
 
 	noStroke();
 	fill(0,155,0);
 	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
 
-	//draw the canyon
+    if(dist(gameChar_x, gameChar_y, coin.x, coin.y) < 26.5)
+    {
+        coin.isPicked = true;
+    }
 
-    isFalling = gameChar_y < floorPos_y;
+    if(!coin.isPicked)
+    {
+        // Coin
+        stroke(0);
+        fill(250, 255, 80);
+        circle(coin.x, coin.y, 30);
+
+        noStroke();
+
+        fill(100, 100, 100, 100);
+        ellipse(coin.x, coin.y + 20, 40, 3);
+    }
+
+	//draw the canyon
+    fill(100, 155, 255);
+    rect(canyon.x, floorPos_y, canyon.size, 144); 
+
+    console.log(dist(gameChar_x, gameChar_y, (canyon.x + canyon.size), canyon.y));
+    fill(0);
+    ellipse((canyon.x + canyon.size), floorPos_y, 10, 10);
+
+    if(dist(gameChar_x, gameChar_y, canyon.x, canyon.y) < 8 || dist(gameChar_x, gameChar_y, (canyon.x + canyon.size), canyon.y) < 5)
+    {
+        isFalling = true;
+        canyon.hasEntered = true;
+    }
 
 	//the game character
-	if(isLeft && isFalling)
+	if(isLeft && (isFalling || isJumping))
 	{
 		// add your jumping-left code
+        console.log(`isLeft : ${isLeft} isJumping: ${isJumping}`);
+
+        //Spine
+        stroke(0);
+        line(gameChar_x, gameChar_y - offset,  gameChar_x, (gameChar_y - offset) - SPINE_LENGTH); 
+
+        //Left leg
+        line(gameChar_x, gameChar_y - offset,  (gameChar_x - (LEG_LENGTH / 2) - 4), gameChar_y - 10); 
+        line((gameChar_x - (LEG_LENGTH / 2) - 4), gameChar_y - 10, (gameChar_x - (LEG_LENGTH / 2) + 2), gameChar_y - 2); 
+
+        //Rigth leg
+        line(gameChar_x, gameChar_y - offset,  gameChar_x + 14, gameChar_y); 
+
+        //Head
+        circle(gameChar_x, gameChar_y - SPINE_LENGTH - HEAD_DIAMETER - 4, HEAD_DIAMETER);
+
+        //Right arm
+        line(gameChar_x, (gameChar_y - offset) - CHEST_LENGTH,  gameChar_x + HAND_LENGTH, (gameChar_y - offset) - CHEST_LENGTH); 
+
+        //Left arm
+        line(gameChar_x, (gameChar_y - offset) - CHEST_LENGTH,  gameChar_x - HAND_LENGTH, (gameChar_y - offset) - CHEST_LENGTH - delta); 
+        //Shadow
+        fill(134);
+        noStroke();
+        ellipse(gameChar_x, gameChar_y + 6, 40,10);
 
 	}
-    else if(isJumping)
-    {
-
-    }
-	else if(isRight && isFalling)
+	else if(isRight && (isFalling || isJumping))
 	{
 		// add your jumping-right code
+        console.log(`isRight : ${isRight} isJumping: ${isJumping}`);
+
+        //Spine
+        stroke(0);
+        line(gameChar_x, gameChar_y - offset,  gameChar_x, (gameChar_y - offset) - SPINE_LENGTH); 
+
+        //Left leg
+        line(gameChar_x, gameChar_y - offset,  gameChar_x - 14, gameChar_y); 
+
+        //Rigth leg
+        line(gameChar_x, gameChar_y - offset,  (gameChar_x + (LEG_LENGTH / 2) + 4), gameChar_y - 10); 
+        line((gameChar_x + (LEG_LENGTH / 2) + 4), gameChar_y - 10, (gameChar_x + (LEG_LENGTH / 2) - 2), gameChar_y - 2); 
+
+        //Head
+        circle(gameChar_x, gameChar_y - SPINE_LENGTH - HEAD_DIAMETER - 4, HEAD_DIAMETER);
+
+        //Right arm
+        line(gameChar_x, (gameChar_y - offset) - CHEST_LENGTH,  gameChar_x + HAND_LENGTH, (gameChar_y - offset) - CHEST_LENGTH - delta); 
+
+        //Left arm
+        line(gameChar_x, (gameChar_y - offset) - CHEST_LENGTH,  gameChar_x - HAND_LENGTH, (gameChar_y - offset) - CHEST_LENGTH); 
+        //Shadow
+        fill(134);
+        noStroke();
+        ellipse(gameChar_x, gameChar_y + 6, 40,10);
 
 	}
 	else if(isLeft && !isRight)
@@ -110,12 +207,38 @@ function draw()
         ellipse(gameChar_x, gameChar_y + 6, 40,10);
 
 	}
-	else if(isFalling || isPlummeting)
+	else if(isFalling || isJumping || canyon.hasEntered)
 	{
 		// add your jumping facing forwards code
+        //Spine
+        stroke(0);
+        line(gameChar_x, gameChar_y - offset,  gameChar_x, (gameChar_y - offset) - SPINE_LENGTH); 
+
+        //Left leg
+        line(gameChar_x, gameChar_y - offset,  (gameChar_x - LEG_LENGTH / 2) - 3, gameChar_y - 6); 
+        line((gameChar_x - LEG_LENGTH / 2) - 3, gameChar_y - 6,  gameChar_x - LEG_LENGTH / 2 , gameChar_y); 
+
+        //Rigth leg
+        line(gameChar_x, gameChar_y - offset,  (gameChar_x + LEG_LENGTH / 2) + 3, gameChar_y - 6); 
+        line((gameChar_x + LEG_LENGTH / 2) + 3, gameChar_y - 6,  gameChar_x + LEG_LENGTH / 2 , gameChar_y); 
+
+        //Head
+        circle(gameChar_x, gameChar_y - SPINE_LENGTH - HEAD_DIAMETER - 4, HEAD_DIAMETER);
+
+        //Right arm
+        line(gameChar_x, (gameChar_y - offset) - CHEST_LENGTH,  gameChar_x + HAND_LENGTH, (gameChar_y - offset) - CHEST_LENGTH - delta); 
+
+        //Left arm
+        line(gameChar_x, (gameChar_y - offset) - CHEST_LENGTH,  gameChar_x - HAND_LENGTH, (gameChar_y - offset) - CHEST_LENGTH - delta); 
+
+        //Shadow
+        fill(134);
+        noStroke();
+        ellipse(gameChar_x, gameChar_y + 6, 40,10);
+
 
 	}
-	else
+	else // Idle
 	{
         //Spine
         stroke(0);
@@ -157,6 +280,26 @@ function draw()
         gameChar_x++;
     }
 
+    // Start adding to the y, so it simulates gravity
+    if(isFalling && (gameChar_y < floorPos_y || canyon.hasEntered))
+    {
+        gameChar_y += 6;
+    }
+    else if(isFalling && gameChar_y == floorPos_y)
+    {
+        isFalling = false;
+    }
+
+
+    if(isJumping && gameChar_y > maximumHeight)
+    {
+        gameChar_y -= 10; // 10 is the force of the jump
+    }
+    else if(gameChar_y == maximumHeight && isJumping)
+    {
+        isJumping = false;
+        isFalling = true;
+    }
 }
 
 
@@ -176,9 +319,7 @@ function keyPressed()
         isLeft = true;
     }
 
-    console.log(keyCode);
-
-    if(keyCode == 88 && !isFalling)
+    if(keyCode == 32 && !isFalling && !isJumping)
     {
         isJumping = true;
     }
